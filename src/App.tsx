f916d6fcbcdf0c5f972443fc3d7f94cddd445c6a
Nikita Lipatov <6stringsohei@gmail.com>
2022-09-13 00:18:08 +0300

    React-tabs added, css-related changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Tree} from 'react-tree-graph';
-import {data} from './data';
+import {data, Species, Specie} from './data';
 import 'react-tree-graph/dist/style.css'
 import './App.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import TabsComponent from './TabsComponent';
 
 const App: React.FC = (props: any) => {
+  const chooseSpecieAssumption: string = "Выберите вид в дереве";
+  const [tabSpecie, setTabSpecie] = useState<Specie>({id: -1, description: chooseSpecieAssumption, existance: chooseSpecieAssumption, facts: [`${chooseSpecieAssumption}`], name: chooseSpecieAssumption});
   const handleClick = (event: any, node: any) => {
-    console.log('handle click ', event);
-    console.log('handle click node', node);
-    console.log(node)
-    alert(`${node} got clicked`);
+    const circles = document.getElementsByClassName("node");
+    for (let i = 0; i < circles.length; i++) {
+      if(circles[i].getElementsByTagName("circle")[0].classList.contains("_active"))
+      {
+        circles[i].getElementsByTagName("circle")[0].classList.remove("_active")
+      }
+    }
+
+    console.log(event.target.classList.add('_active'))
+    // console.log('handle click ', event);
+    // console.log('handle click node', node);
+
+    const targetSpecie = Species.filter(x=> x.name === node);
+    // console.log(targetSpecie[0] === undefined)
+    // console.log(targetSpecie[0] !== undefined ? targetSpecie[0].facts : 'N/A')
+
+    setTabSpecie(targetSpecie[0]);
+
+    // console.log(tabSpecie);
+    
+    // alert(`${node} got clicked`);
   }
 
   return (
@@ -17,18 +39,23 @@ const App: React.FC = (props: any) => {
       <div className='tree'>
         <Tree
         data={data}
-        height={800}
+        height={900}
         width={800}
-        margins={{ top: 50, bottom: 0, left: 10, right: 200 }}
+        margins={{ top: 100, bottom: 100, left: 100, right: 100 }}
         gProps={{
           className: 'node',
           onClick: handleClick,
+        }}
+        svgProps={{
+          viewBox: "50 100 800 600"
         }}/>
       </div>
-      <div>
-        <div>
-          
-        </div>
+      <div className="information">
+        <TabsComponent description={tabSpecie.description}
+        existance={tabSpecie.existance}
+        facts={tabSpecie.facts}
+        name={tabSpecie.name}
+        id={tabSpecie.id}/>
       </div>
     </div>
   );
---
+++ src/TabsComponent.tsx
@@ -0,0 +1,32 @@
+import { FC, useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Specie } from './data';
+
+const TabsComponent: FC<Specie> = (prop): JSX.Element => {
+    useEffect(() => {
+        console.log('render tabs')
+    }, [prop]);
+    return(
+        <div style={{width: '100%'}}>
+        <Tabs>
+            <TabList>
+              <Tab>Описание</Tab>
+              <Tab>Факты</Tab>
+              <Tab>Наличие в природе</Tab>
+            </TabList>
+
+            <TabPanel>
+              <p>{prop.description}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{prop.facts}</p>
+            </TabPanel>
+            <TabPanel>
+              <p>{prop.existance}</p>
+            </TabPanel>
+          </Tabs>
+        </div>
+    );
+}
+
+export default TabsComponent;
